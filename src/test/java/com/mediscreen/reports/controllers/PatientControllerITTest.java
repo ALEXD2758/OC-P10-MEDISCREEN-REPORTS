d51550af4b9afd959cf02f642c3ce4d4c2c45d5f
Alexandre Dubois <alex_2758@hotmail.com>
2021-02-24 22:30:49 +0100

    Implementation static and template structure in resources folder for home.html and patient/list.html Implementation integration test for home view and Patient/list view in two classes: HomeControllerITTest and PatientControllerITTest

---
+++ src/test/java/com/mediscreen/reports/controllers/PatientControllerITTest.java
@@ -0,0 +1,90 @@
+package com.mediscreen.reports.controllers;
+
+import com.mediscreen.reports.model.AddressModel;
+import com.mediscreen.reports.model.PatientModel;
+import com.mediscreen.reports.service.webclient.PatientWebClientService;
+import org.joda.time.LocalDate;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PatientControllerITTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webContext;
+
+    @MockBean
+    PatientWebClientService patientWebClientService;
+
+
+    @Before
+    public void setupMockmvc() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webContext).build();
+    }
+
+    public AddressModel addressModel1() {
+        AddressModel addressModel1 = new AddressModel();
+        addressModel1.setStreet("StreetTest1");
+        addressModel1.setCity("CityTest1");
+        addressModel1.setPostcode("112345");
+        addressModel1.setDistrict("DistrictTest1");
+        addressModel1.setState("StateTest1");
+        addressModel1.setCountry("CountryTest1");
+        return addressModel1;
+    }
+
+    public PatientModel patientModel1() {
+
+        LocalDate date = new LocalDate(2020, 01, 01);
+        PatientModel patientModel1 = new PatientModel();
+        patientModel1.setGivenName("John");
+        patientModel1.setFamilyName("Boyd");
+        patientModel1.setBirthdate(date);
+        patientModel1.setGender("MALE");
+        patientModel1.setAddress(addressModel1());
+        patientModel1.setEmailAddress("EmailTest1@email.com");
+        patientModel1.setPhoneNumber("004678925899");
+        return patientModel1;
+    }
+
+    @Test
+    public void getRequestPatientListViewShouldReturnSuccess() throws Exception {
+        //1. Setup
+        List<PatientModel> patientList = new ArrayList<>();
+        patientList.add(patientModel1());
+
+        doReturn(patientList)
+                .when(patientWebClientService)
+                .getListPatients();
+
+        //2. Act
+        mockMvc.perform(get("/patient/list"))
+
+        //3. Assert
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("patient/list"))
+                .andExpect(model().attributeExists("patients"))
+                .andReturn();
+        assertTrue(patientList.get(0).getGivenName().equals("John"));
+    }
+}
